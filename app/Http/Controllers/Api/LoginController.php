481c6e41319d2b23d5f32ebcf7acd6cc88ca3480
Tomas Diaz <tdiaz@banksa.com.ar>
2022-10-21 20:15:50 -0300

    Login corregido

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -20,46 +20,55 @@ class LoginController extends Controller
            
            $embarcaciones_aceptada = array();
            $capitan = Capitan::where('usuario', $req -> usuario)->first();
-           $embarcaciones = DB::table('embarcacion')
-                              ->join('capitanembarcacion', 'embarcacion.IdEmbarcacion', '=', 'capitanembarcacion.IdEmbarcacion')
-                              ->where('capitanembarcacion.IdCapitan', $capitan -> id)
-                              ->get();
 
-           foreach( $embarcaciones as $embarcacion){
-
-                $capitan_armador =  capitan_armador::where('id_armador', $embarcacion -> IdArmador)->where('id_capitan', $capitan->id)->first();
-
-                if(!empty($capitan_armador) && $capitan_armador -> estado == 1){
-
-                    $embarcaciones_aceptada[] = [
-                        "IdEmbarcacion"=> $embarcacion -> IdEmbarcacion,
-                        "IdArmador" => $embarcacion -> IdArmador,
-                        "Nombre" => $embarcacion -> Nombre . " - " . User::find($capitan_armador -> id_armador) -> empresa,
-                        "Matricula" => $embarcacion -> Matricula,
-                        "PermisoPesca" => $embarcacion -> PermisoPesca,
-                        "FechaVigenciaPermisoPesca" => $embarcacion -> FechaVigenciaPermisoPesca,
-                        "Estado" => $embarcacion -> Estado,
-                        "FechaRegistro" => $embarcacion -> FechaRegistro,
-                        "Pais" => $embarcacion -> Pais,
-                        "id_tipo_barco" => $embarcacion -> id_tipo_barco,
-                        "IdCapitan" => $embarcacion -> IdCapitan,
-                        "FechaIngreso" => $embarcacion -> FechaIngreso
-                    ];
-                }
-
+           if(!empty($capitan)){
+               $embarcaciones = DB::table('embarcacion')
+                                  ->join('capitanembarcacion', 'embarcacion.IdEmbarcacion', '=', 'capitanembarcacion.IdEmbarcacion')
+                                  ->where('capitanembarcacion.IdCapitan', $capitan -> id)
+                                  ->get();
+    
+               foreach( $embarcaciones as $embarcacion){
+    
+                    $capitan_armador =  capitan_armador::where('id_armador', $embarcacion -> IdArmador)->where('id_capitan', $capitan->id)->first();
+    
+                    if(!empty($capitan_armador) && $capitan_armador -> estado == 1){
+    
+                        $embarcaciones_aceptada[] = [
+                            "IdEmbarcacion"=> $embarcacion -> IdEmbarcacion,
+                            "IdArmador" => $embarcacion -> IdArmador,
+                            "Nombre" => $embarcacion -> Nombre . " - " . User::find($capitan_armador -> id_armador) -> empresa,
+                            "Matricula" => $embarcacion -> Matricula,
+                            "PermisoPesca" => $embarcacion -> PermisoPesca,
+                            "FechaVigenciaPermisoPesca" => $embarcacion -> FechaVigenciaPermisoPesca,
+                            "Estado" => $embarcacion -> Estado,
+                            "FechaRegistro" => $embarcacion -> FechaRegistro,
+                            "Pais" => $embarcacion -> Pais,
+                            "id_tipo_barco" => $embarcacion -> id_tipo_barco,
+                            "IdCapitan" => $embarcacion -> IdCapitan,
+                            "FechaIngreso" => $embarcacion -> FechaIngreso
+                        ];
+                    }
+    
+               }
+                
+       
+               if (Hash::check( $req -> clave, $capitan -> clave)) {
+                   return  response()->json(['msj' => 'Acceso permitido', 'capitan' => new CapitanResource($capitan), 'embarcaciones' => $embarcaciones_aceptada],200);
+               }
+       
+               return  response()->json(['msj' => 'No autorizado' ],401);
            }
-            
-   
-           if (Hash::check( $req -> clave, $capitan -> clave)) {
-               return  response()->json(['msj' => 'Acceso permitido', 'capitan' => new CapitanResource($capitan), 'embarcaciones' => $embarcaciones_aceptada],200);
-           }
-   
+
            return  response()->json(['msj' => 'Capitan no econtrado' ],404);
 
        } 
        catch (\Exception $e) {
-        report($e);
-        return response()->json(['msj'=>'Server error','err'=>$e],500);
+        $errores_lista =[
+            'error'=>$e->getMessage(),
+            'line'=>$e->getline(),
+            'file'=> $e->getfile(),
+        ];
+        return response()->json(['msj'=>'Server error','err'=>$errores_lista],500);
        }
 
 
